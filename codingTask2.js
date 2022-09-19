1a26150d2dcee66f985f5b122fe34b52fb733119
Yunosuke Sato <yunosukesato04@gmail.com>
2022-09-19 16:28:00 -0700

    added coding Task2

--- codingTask2.js
+++ codingTask2.js
@@ -3,45 +3,44 @@ let MatScore = 80;
 let SimonScore = 45;
 let lowerLimit = 51;
 
-if(MatScore >= 51 && SimonScore >= 51) {
+if (MatScore >= 51 && SimonScore >= 51) {
   console.log(`Mat's score is ${MatScore}, Simon's score is ${SimonScore}`);
-}else if(MatScore >= 51 || SimonScore >= 51) {
+} else if (MatScore >= 51 || SimonScore >= 51) {
   console.log(`Mat's score is ${MatScore}, Simon's score is ${SimonScore}`);
-  if(MatScore >= 51) {
+  if (MatScore >= 51) {
     console.log(`Mat is passed. His score is ${MatScore}.`);
-  }else if(SimonScore >= 51) {
+  } else if (SimonScore >= 51) {
     console.log(`Simon is passed. His score is ${SimonScore}.`);
   }
-}else{
+} else {
   console.log('Both students are failed');
-};
+}
 
 // Task 2
 let students = [
   ['John', 47, 46],
   ['Bob', 23, 24],
   ['Nick', 40, 35],
-  ['Alex', 44, 45]
-]
+  ['Alex', 44, 45],
+];
 
 const degrees = ['A', 'B', 'C', 'D', 'E'];
 const passingLimit = [91, 81, 71, 61, 51];
 
 function culcTotal(name, score1, score2) {
-    let totalScore = score1 + score2;
-    console.log(`${name} is got ${totalScore}`);
-    return totalScore;
-  };
-
+  let totalScore = score1 + score2;
+  console.log(`${name} is got ${totalScore}`);
+  return totalScore;
+}
 
 function checkDegree(students) {
-  for(i = 0; i < students.length; i++) {
+  for (i = 0; i < students.length; i++) {
     let total = culcTotal(students[i][0], students[i][1], students[i][2]);
-    for(j = 0; j < passingLimit.length; j++) {
-      if(total >= passingLimit[j]) {
+    for (j = 0; j < passingLimit.length; j++) {
+      if (total >= passingLimit[j]) {
         console.log(`${students[i][0]}'s degree is ${degrees[j]}.`);
         break;
-      }else if(total <= 51){
+      } else if (total <= 51) {
         console.log(`${students[i][0]} couldn't get degree`);
         break;
       }
@@ -50,5 +49,3 @@ function checkDegree(students) {
 }
 
 checkDegree(students);
-
-
