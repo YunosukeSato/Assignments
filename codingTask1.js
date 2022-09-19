8692b9a9ae4da64462457b5bcfa6d3952eb96641
Yunosuke Sato <yunosukesato04@gmail.com>
2022-09-19 16:42:11 -0700

    added coding task1

--- codingTask1.js
+++ codingTask1.js
@@ -2,45 +2,45 @@
 
 function studentGrade(score) {
   switch (true) {
-    case(score == 100):
-      console.log("perfect score");
+    case score == 100:
+      console.log('perfect score');
       break;
-    case(score >= 70):
-      console.log("passed");
+    case score >= 70:
+      console.log('passed');
       break;
-    case(score > 0):
-      console.log("failed");
+    case score > 0:
+      console.log('failed');
       break;
-    case(score <= 0):
-      console.log("invalid score");
+    case score <= 0:
+      console.log('invalid score');
       break;
     default:
-      console.log("invalid score");
+      console.log('invalid score');
       break;
-  };
+  }
 }
 
-studentGrade(50)
+studentGrade(50);
 
 // Task 2
 
 function numEvenOdd(num) {
-  switch(true) {
-    case(num % 2 == 0):
-      console.log("even");
+  switch (true) {
+    case num % 2 == 0:
+      console.log('even');
       break;
-    case(num % 2 == 1):
-      console.log("odd");
+    case num % 2 == 1:
+      console.log('odd');
       break;
-    case(num == 0):
-      console.log("even");
+    case num == 0:
+      console.log('even');
       break;
-    case(num < 0):
-      console.log("invalid number");
+    case num < 0:
+      console.log('invalid number');
       break;
     default:
-      console.log("invalid number");
-  };
+      console.log('invalid number');
+  }
 }
 
 numEvenOdd(48);
@@ -49,19 +49,19 @@ numEvenOdd(48);
 
 function numPosNeg(num2) {
   switch (true) {
-    case(num2 > 0):
-      console.log("positive");
+    case num2 > 0:
+      console.log('positive');
       break;
-    case(num2 < 0):
-      console.log("negative");
+    case num2 < 0:
+      console.log('negative');
       break;
-    case(num2 == 0):
-      console.log("zero");
+    case num2 == 0:
+      console.log('zero');
       break;
     default:
-      console.log("invalid number");
+      console.log('invalid number');
       break;
-  };
+  }
 }
 
 numPosNeg(0);
@@ -69,13 +69,13 @@ numPosNeg(0);
 // Task 4
 
 function personAge(age) {
-  if(age <= 19 && age > 0) {
-    console.log("minor");
-  }else if(age > 19) {
-    console.log("adult");
-  }else{
-    console.log("invalid person");
-  };
+  if (age <= 19 && age > 0) {
+    console.log('minor');
+  } else if (age > 19) {
+    console.log('adult');
+  } else {
+    console.log('invalid person');
+  }
 }
 
 personAge(19);
@@ -83,11 +83,11 @@ personAge(19);
 // Task 5
 
 function concatStr(str1, str2) {
-  if(typeof str1 === "string" && typeof str2 === "string") {
-    console.log(str1 + " " + str2);
-  }else{
-    console.log("invalid strings");
-  };
+  if (typeof str1 === 'string' && typeof str2 === 'string') {
+    console.log(str1 + ' ' + str2);
+  } else {
+    console.log('invalid strings');
+  }
 }
 
-concatStr("hello", "world");
+concatStr('hello', 'world');
