1e2dedd7aead3a9e2a2501a27149ef882da2db1a
Yunosuke Sato <yunosukesato04@gmail.com>
2022-09-26 14:22:00 -0700

    added binary search

--- Binary_Search.js
+++ Binary_Search.js
@@ -1,18 +1,18 @@
 function binarySearch(arr, key) {
   let leftPoint = 0;
   let rightPoint = arr.length - 1;
-  while(leftPoint <= rightPoint) {
+  while (leftPoint <= rightPoint) {
     let mid = Math.floor((leftPoint + rightPoint) / 2);
     let element = arr[mid];
-    if(element < key) {
+    if (element < key) {
       leftPoint = mid + 1;
-    }else if(element > key) {
+    } else if (element > key) {
       rightPoint = mid - 1;
-    }else{
+    } else {
       return mid;
     }
   }
   return -1;
 }
 
-console.log(binarySearch([1,2,3,4,5,6], 5));
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 5));
